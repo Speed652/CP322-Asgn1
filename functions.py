be33e39bca298e40d0abdc5e8dc42ce39379d8c1
Mohamed El Sayed <102246128+Speed652@users.noreply.github.com>
2023-10-15 19:40:13 -0400

    update

--- functions.py
+++ functions.py
@@ -1,13 +1,13 @@
-from scipy.spatial.distance import euclidean
+import pandas as pd
 import numpy as np
 
 class LogisticRegression:
-    def init(self, learning_rate=0.01, num_iterations=1000):
+    def __init__(self, learning_rate=0.01, num_iterations=1000):
         self.learning_rate = learning_rate
         self.num_iterations = num_iterations
 
     def sigmoid(self, z):
-        return 1 / (1 + np.exp(-z))
+        return (1 / (1 + np.exp(-z)))
 
     def fit(self, X, y):
         # Initialize weights and bias
@@ -32,6 +32,40 @@ class LogisticRegression:
         predictions = self.sigmoid(z)
         return np.round(predictions)
     
+    def cross_validate(self, X, y, num_folds=5):
+        # Define the number of folds
+        fold_size = len(X) // num_folds
+
+        # Initialize a list to store performance metrics (e.g., accuracy) for each fold
+        metrics = []
+
+        for i in range(num_folds):
+            # Define the indices for the validation set
+            start_idx = i * fold_size
+            end_idx = (i + 1) * fold_size
+
+            # Split the data into training and validation sets
+            X_train = np.vstack((X[:start_idx], X[end_idx:]))
+            y_train = np.concatenate((y[:start_idx], y[end_idx:]))
+            X_val = X[start_idx:end_idx]
+            y_val = y[start_idx:end_idx]
+
+            # Initialize your logistic regression model
+            model = LogisticRegression()
+            model.fit(X_train, y_train)
+
+            # Make predictions on validation set
+            y_pred = model.predict(X_val)
+
+            # Evaluate the model using a performance metric 
+            accuracy = np.mean(y_pred == y_val)
+
+            # Store the metric for this fold
+            metrics.append(accuracy)
+
+        # Calculate the average performance across all folds
+        average_performance = sum(metrics) / len(metrics)
+        return average_performance
 
 class KNearestNeighbor:
     def __init__(self, k=5):
@@ -45,13 +79,48 @@ class KNearestNeighbor:
         predictions = []
 
         for x in X_test:
-            distances = [euclidean(x, x_train) for x_train in self.X_train]
+            distances = [np.linalg.norm(x - x_train) for x_train in self.X_train]  # Calculate Euclidean distance
             sorted_indices = np.argsort(distances)[:self.k]
             k_nearest_labels = [self.y_train[i] for i in sorted_indices]
             predictions.append(max(set(k_nearest_labels), key=k_nearest_labels.count))
 
-        return np.array(predictions)
 
+        return np.array(predictions)
+    
+    def cross_validate(self, X, y, num_folds=5):
+        # Define the number of folds
+        fold_size = len(X) // num_folds
+
+        # Initialize a list to store performance metrics for each fold
+        metrics = []
+
+        for i in range(num_folds):
+            # Define the indices for the validation set
+            start_idx = i * fold_size
+            end_idx = (i + 1) * fold_size
+
+            X_train = np.vstack((X[:start_idx], X[end_idx:]))
+            y_train = np.concatenate((y[:start_idx], y[end_idx:]))
+            X_val = X[start_idx:end_idx]
+            y_val = y[start_idx:end_idx]
+
+            # Initialize your KNearestNeighbor model
+            model = KNearestNeighbor()
+            model.fit(X_train, y_train)
+
+            # Make predictions on validation set
+            y_pred = model.predict(X_val)
+
+            # Evaluate the model 
+            accuracy = np.mean(y_pred == y_val)
+            metrics.append(accuracy)
+
+        # Calculate the average performance from folds
+        average_performance = sum(metrics) / len(metrics)
+        return average_performance
+    
+    
+    
 def clean_data(data):
     # Remove examples with missing or malformed features
     clean_data = []
@@ -67,10 +136,13 @@ def clean_data(data):
     for idx in categorical_indices:
         unique_values = np.unique(clean_data[:, idx])
         for val in unique_values:
-            one_hot_encoded = (clean_data[:, idx] == val).astype(np.int)
+            one_hot_encoded = (clean_data[:, idx] == val).astype(np.int32)
             clean_data = np.hstack((clean_data, one_hot_encoded.reshape(-1, 1)))
 
     # Remove the original categorical column after one-hot encoding
     clean_data = np.delete(clean_data, categorical_indices, axis=1)
 
     return clean_data
+
+
+    
--- main.py
+++ main.py
@@ -1,15 +1,77 @@
 import pandas as pd
 import numpy as np
+from functions import LogisticRegression
+from functions import KNearestNeighbor
+from functions import clean_data
 
-ionosphere_data = pd.read_csv('ionosphere\ionosphere.data')
-adult_data = pd.read_csv('adult\adult.data')
-heart_disease_data = pd.read_csv('heart_disease\cleveland.data')
-iris_data = pd.read_csv('iris\iris.data')
 
+ionosphere_data = pd.read_csv('ionosphere/ionosphere.data', encoding='latin1')
+adult_data = pd.read_csv('adult/adult.data', encoding='latin1')
+heart_disease_data = pd.read_csv('heart_disease/cleveland.data', encoding='latin1')
+iris_data = pd.read_csv('iris/iris.data', encoding='latin1')
 
 #Convert the dataset to a NumPy array
 ionosphere_np = ionosphere_data.values
 adult_data_np = adult_data.values
 heart_disease_np = heart_disease_data.values
 iris_np = iris_data.values
+
+
+
+ionosphere_X = ionosphere_np[:, :-1]
+ionosphere_y = ionosphere_np[:, -1]
+
+adult_X = adult_data_np[:, :-1]
+adult_y = adult_data_np[:, -1]
+
+heart_disease_X = heart_disease_np[:, :-1]
+heart_disease_y = heart_disease_np[:, -1]
+
+iris_X = iris_np[:, :-1]
+iris_y = iris_np[:, -1]
+
+# Initialize logistic regression model
+lr_model = LogisticRegression()
+
+# Perform 5-fold cross-validation for logistic regression on ionosphere dataset
+ionosphere_X_cleaned = clean_data(ionosphere_X)
+average_accuracy_lr_ionosphere = lr_model.cross_validate(ionosphere_X_cleaned, ionosphere_y)
+print(f'Average Accuracy for Logistic Regression on Ionosphere dataset: {average_accuracy_lr_ionosphere:.4f}')
+
+# Perform 5-fold cross-validation for logistic regression on adult dataset
+adult_X_cleaned = clean_data(adult_X)
+average_accuracy_lr_adult = lr_model.cross_validate(adult_X_cleaned, adult_y)
+print(f'Average Accuracy for Logistic Regression on Adult dataset: {average_accuracy_lr_adult:.4f}')
+
+# Perform 5-fold cross-validation for logistic regression on heart disease dataset
+heart_disease_X_cleaned = clean_data(heart_disease_X)
+average_accuracy_lr_heart = lr_model.cross_validate(heart_disease_X_cleaned, heart_disease_y)
+print(f'Average Accuracy for Logistic Regression on Heart Disease dataset: {average_accuracy_lr_heart:.4f}')
+
+# Perform 5-fold cross-validation for logistic regression on iris dataset
+iris_X_cleaned = clean_data(iris_X)
+average_accuracy_lr_iris = lr_model.cross_validate(iris_X_cleaned, iris_y)
+print(f'Average Accuracy for Logistic Regression on Iris dataset: {average_accuracy_lr_iris:.4f}')
+
+# Initialize k-Nearest Neighbor model
+knn_model = KNearestNeighbor()
+
+# Perform 5-fold cross-validation for k-Nearest Neighbor on ionosphere dataset
+average_accuracy_knn_ionosphere = knn_model.cross_validate(ionosphere_X_cleaned, ionosphere_y)
+print(f'Average Accuracy for k-Nearest Neighbor on the Ionosphere dataset: {average_accuracy_knn_ionosphere:.4f}')
+
+# Perform 5-fold cross-validation for k-Nearest Neighbor on the adult dataset
+average_accuracy_knn_adult = knn_model.cross_validate(adult_X_cleaned, adult_y)
+print(f'Average Accuracy for k-Nearest Neighbor on the Adult dataset: {average_accuracy_knn_adult:.4f}')
+
+# Perform 5-fold cross-validation for k-Nearest Neighbor on the heart disease dataset
+average_accuracy_knn_heart = knn_model.cross_validate(heart_disease_X_cleaned, heart_disease_y)
+print(f'Average Accuracy for k-Nearest Neighbor on the Heart Disease dataset: {average_accuracy_knn_heart:.4f}')
+
+# Perform 5-fold cross-validation for k-Nearest Neighbor on the iris dataset
+average_accuracy_knn_iris = knn_model.cross_validate(iris_X_cleaned, iris_y)
+print(f'Average Accuracy for k-Nearest Neighbor on the Iris dataset: {average_accuracy_knn_iris:.4f}')
+
+
+
 
